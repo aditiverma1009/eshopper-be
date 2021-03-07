0b70dbdb7e515d80a679992021466fc6041d518d
Aditi Verma <aditiverma1009@gmail.com>
2021-03-07 16:40:52 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 8080
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/', function (req, res) {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
